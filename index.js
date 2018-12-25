ff5369c8d2a69aaabd118498dc3a254696e58c95
Julia <julia@Manels-MacBook-Air.local>
2018-12-25 16:10:28 +0100

    removing logs

--- index.js
+++ index.js
@@ -6,20 +6,16 @@ const log = require('./utilities/logger');
 
 const bitfinex = new ccxt.bitfinex();
 
-console.log('process ', process.env)
-
 let counter = 1;
 
 const startPolling = async () => {
     try {
         let bitfinex_prices = await bitfinex.fetchTicker('BTC/USD');
         log.info(`--> REQUEST ${counter}: `, bitfinex_prices);
-        console.info(`--> REQUEST ${counter}: `, bitfinex_prices);
     } catch(err) {
         log.error(`--> REQUEST ${counter}: `, err);
-        console.error(`--> REQUEST ${counter}: `, err);
     }
     counter++;
 };
 
-setInterval(startPolling, 20000);
+setInterval(startPolling, 300000);
